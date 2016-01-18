7f43863597ae53084930f549ef426f3eee7040e8
Jeff Wakeman <deftwun@gmail.com>
2016-01-18 21:56:33 +0000

    MongoDB connect

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ var port = process.env.PORT || 8080;
 
 var mongo = require('mongodb').MongoClient;
 
-app.use("/",express.static("client"));
-
-app.get("/new/:url",require("./routes/new.js"));
-
-app.get("/:url",require("./routes/resolve.js"));
-
-app.listen(port, function () {
-  console.log('url-shortener running on port ' + port);
+mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+  
+  if (err) {
+      throw new Error('Database failed to connect!');
+  } else {
+      console.log('MongoDB successfully connected on port 27017.');
+  }
+    
+  app.use("/",express.static("client"));
+  app.get("/new/:url",require("./routes/new.js"));
+  app.get("/:url",require("./routes/resolve.js"));
+  
+  app.listen(port, function () {
+    console.log('url-shortener running on port ' + port);
+  });
+  
 });
